cc4f283aaed740fd7779907fcc021e14083ec442
localuser <allan.asis@gmail.com>
2019-07-18 16:22:16 +0800

    version 20190718 release

--- app.h
+++ app.h
@@ -17,7 +17,7 @@ constants
 ------------------------------------------------------------------------------------------ */
 #define DELIMITER ':'
 #define JOBFILE "JOBFILE"
-#define VERSION "beta.2.0.20190710"
+#define VERSION "beta.2.0.20190718"
 #define DEVELOPER "allan asis / allan.asis@gmail.com"
 #define MAXCONNECT 20
 #define KILLAPPCMD "kill.app.sh"
